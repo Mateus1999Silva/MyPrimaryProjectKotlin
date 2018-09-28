e373d36a76c5d653dfd7d4a3dbfa75d902c21554
Mateus1999Silva <mateusoliveira@brq.com>
2018-09-28 10:43:23 -0300

    version2

--- app/src/main/java/com/example/mateusoliveira/projectkotlin/Welcome.kt
+++ app/src/main/java/com/example/mateusoliveira/projectkotlin/Welcome.kt
@@ -6,16 +6,19 @@ import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import kotlinx.android.synthetic.main.welcome.*
 
-class Welcome:AppCompatActivity(){
+class Welcome : AppCompatActivity() {
     private var stringBuilder = StringBuilder();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome)
 
-        val nomeDado : String= intent.getStringExtra("nome")
-        val cpfDado : String= intent.getStringExtra("cpf")
-        dados.text = stringBuilder.append(cpfDado).append(nomeDado)
+        val nomeDado: String = intent.getStringExtra("nome")
+        val cpfDado: String = intent.getStringExtra("cpf")
+        dados.text = stringBuilder.append("\n")
+                .append(cpfDado)
+                .append("\n")
+                .append(nomeDado);
     }
 
 }
